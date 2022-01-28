e720ecfda5d2d92c8aea2c2e5c951adaee79b705
Tameka Gray <tsgray01@hotmail.com>
2022-01-27 21:47:35 -0500

    Using Redux

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { Text, ScrollView, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
-import { FlatList } from 'react-native-gesture-handler';
-import { PARTNERS } from '../shared/partners';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+    return {
+        partners: state.partners
+    };
+};
+
 
 function Mission() {   
         return (
@@ -20,14 +27,7 @@ function Mission() {
 
 
 class About extends Component {
-    
-    constructor(props) {
-        super(props);
-        this.state = {
-           partners: PARTNERS
-        };
-    }
-
+      
     static navigationOptions = {
         title: 'About Us'
     }
@@ -38,7 +38,7 @@ render() {
             <ListItem
                 title={item.name}
                 subtitle={item.description}
-                leftAvatar={{source: require('./images/bootstrap-logo.png')}}
+                leftAvatar={{source: {uri: baseUrl + item.image}}}
             />
         );
     };
@@ -50,7 +50,7 @@ render() {
            <Card
                 title="Community Partners">
                 <FlatList
-                    data={this.state.partners}
+                    data={this.props.partners.partners}
                     renderItem={renderPartner}
                     keyExtractor={item => item.id.toString()}
                     />
@@ -64,4 +64,4 @@ render() {
     }
 
 
-export default About;
+export default connect(mapStateToProps)(About);
