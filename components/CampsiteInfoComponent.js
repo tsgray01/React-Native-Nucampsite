9124e3dff6c7134ac77bf6826ef0cf8c60b13532
Tameka Gray <tsgray01@hotmail.com>
2022-02-09 20:00:03 -0500

    Social Sharing

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList, Modal, Button, StyleSheet, Alert, PanResponder } from 'react-native';
+import { Text, View, ScrollView, FlatList, Modal, Button, StyleSheet, Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
 import { connect } from 'react-redux';
@@ -64,6 +64,16 @@ function RenderCampsite(props) {
         }
     });
 
+    const shareCampsite = (title, message, url) => {
+        Share.share({
+            title: title,
+            message: `${title}: ${message} ${url}`,
+            url: url
+        },{
+            dialogTitle: 'Share ' + title
+        });
+    };
+
 
     if (campsite) {
         return (
@@ -94,6 +104,14 @@ function RenderCampsite(props) {
                         reverse
                         onPress={() => props.onShowModal()}
                         />
+                        <Icon
+                        name={'share'}
+                        type='font-awesome'
+                        color='#5637DD'
+                        raised
+                        reverse
+                        onPress={() => shareCampsite(campsite.name, campsite.description, baseUrl + campsite.image)} 
+                    />
                     </View>
                 </Card>
             </Animatable.View>
